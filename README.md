94b9ac41a82bc8b0fba8a376f694e6997c07e53d
ekosh02 <polumontum@gmail.com>
2026-01-27 19:40:37 +0500

    init project

---
+++ README.md
@@ -0,0 +1,3 @@
+# flutter_the_movie_db
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/flutter_the_movie_db/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.flutter_the_movie_db
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
